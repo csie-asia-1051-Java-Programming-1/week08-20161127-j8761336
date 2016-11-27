d7332c74e935bd2ccedb5a9aa76bc563cb65bcf6
j8761336 <105021026@live.asia.edu.tw>
2016-11-28 01:52:48 +0800

    00

--- src/hw/hw02.java
+++ src/hw/hw02.java
@@ -1,14 +1,30 @@
 package hw;
+
+import java.util.Scanner;
+
 /*
  * Topic: 將前一題加強成可以做10以下的各種數字系統轉換 (必須使用遞迴)
  * Date: 2016/11/21
- * Author: 1050210XX 周永振老師
+ * Author: 105021026 詹佩郡
  */
 public class hw02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scn =new Scanner(System.in);
+		System.out.print("將值轉換成n進制:(先輸入n再輸入值)");
+		int a =scn.nextInt();
+		int b =scn.nextInt();
+		fun(a,b);
 	}
+	public static void fun(int x,int y){
+		String str =" ";
+		while(y!=0){
+			str =y%x+str;
+			y=y/x;
+		}
+		if(y==0){
+			System.out.print(str);
+		}
+	}}
 
-}
--- src/hw/hw03.java
+++ src/hw/hw03.java
@@ -1,14 +1,32 @@
 package hw;
+import java.util.Scanner;
+
 /*
  * Topic: 請寫一個遞迴程式，輸入一個 int 型態的資料，並回傳該資料在二進制下有多少個位元是 1，例如: 輸入 13 得到結果為 3
  * Date: 2016/11/21
- * Author: 1050210XX 周永振老師
+ * Author: 105021026 詹佩郡
  */
 public class hw03 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scn =new Scanner(System.in);
+		int a =scn.nextInt();
+		String str=" ";
+		int x=0;
+		while(a!=0){
+			str=a%2+str;
+			a=a/2;
+			}
+		if(a==0){
+			for(int i=0;i<str.length();i++){
+				int intValue = Integer.parseInt("str");
+				if(intValue%10==1){
+				x++;
+				}
+				intValue=intValue/10;
+			}System.out.print(x);
+		}
 	}
 
 }
